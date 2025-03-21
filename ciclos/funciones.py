e410aa0cc64ebcfa4fa31f48b42318296c986308
Marina-Mendoza02 <marinams@lcg.unam.mx>
2025-03-21 13:59:35 -0600

    ejemplos con funciones

---
+++ ciclos/funciones.py
@@ -0,0 +1,30 @@
+"""
+secuencia = "GTTCGACTA"
+
+for i, base in enumerate(secuencia):
+    print(f"Posicion {i} : {base}")"
+
+"""
+"""
+# funcion zip
+
+bases = "ATGC"
+complementos = "TACG"
+
+for base, complemento in zip(bases, complementos):
+    print(f"{base} --> {complemento}")"
+
+"""
+
+"""
+genes = ["thrL", "thrA", "thrB"]
+longitudes = [66, 2463]
+
+for gen, tamano in zip(genes, longitudes):
+    print(f"Gen: {gen}, Tamano: {longitudes}")"
+
+"""
+
+# while
+
+
